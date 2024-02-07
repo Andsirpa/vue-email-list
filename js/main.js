f1dab7465f95a1730f26788ffde42fdfdcf47b57
Andsirpa <andsirpa94@gmail.com>
2024-02-07 13:27:13 +0100

    feat: created mail

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const { createApp } = Vue;
 
 const app = createApp({
+    data() {
+        return {
+            title: 'Generate Random Mail',
+            mailList: [],
+            requiredMails: 10,
+
+        };
+    },
+
+    methods: {},
+
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+            const mail = response.data.response;
+            this.mailList.push(mail);
+        })
+    },
 
 });
 app.mount('#app');
